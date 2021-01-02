3e137044e23cbd80f7a1f4724594c8b5a72845ec
extensionmedia <elmeftouhi@gmail.com>
2021-01-02 19:27:05 +0100

    sqlite with data

--- app/Http/Controllers/TirageController.php
+++ app/Http/Controllers/TirageController.php
@@ -11,7 +11,7 @@ use App\Models\Tirage;
 
 class TirageController extends Controller{
     public function index(Request $r){
-        $my_numbers = explode('-', Tirage::find(1)->tirage);
+        $my_numbers = explode('-', Tirage::find(1)->toArray()["tirage"]);
         if($r->has('number')){
             return view('tirage')->with(['my_numbers'=>$my_numbers,'number'=> $r->input('number'),'tirage'=>$this->tirage(2020, $r->input('number'))]);
         }else{
@@ -23,7 +23,7 @@ class TirageController extends Controller{
     public function save(){
         Tirage::truncate();
         $tirage = Tirage::create([
-            'tirage'    =>  "4-3-1-8-43-18-5-16-13-18-14-10-40-49-38-37-30-48"
+            'tirage'    =>  "1-3-4-8-18-43-1-3-4-8-34-48-2-11-12-18-29-30-1-7-20-23-30-49-8-18-19-32-36-48-3-13-15-22-38-39-1-9-14-28-29-42-6-15-20-25-34-42-6-15-42-44-48-49-14-16-18-24-37-39"
         ]);
         dump($tirage);
     }
